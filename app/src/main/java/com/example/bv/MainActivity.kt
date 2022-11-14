8f87834ab4fc338df2f71cd90d1b578037318b66
hoan99 <tuoimeo.99@gmail.com>
2022-11-14 10:15:45 +0700

    init pj

---
+++ app/src/main/java/com/example/bv/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bv
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private val baseUrl = "http://192.168.1.6:3000"
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = getString(R.string.app_name)
+        webView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = Client()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.loadUrl(baseUrl)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    class Client : WebViewClient() {
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            return false
+        }
+    }
+}
