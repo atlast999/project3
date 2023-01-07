272745abb6d066a3274712b9fb4f25c1eda7414c
hoan99 <tuoimeo.99@gmail.com>
2023-01-07 15:06:30 +0700

    toolbar

--- app/src/main/java/com/example/webtoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/webtoapp/ui/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.webtoapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.webtoapp.R
+import com.example.webtoapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title = getString(R.string.app_name)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
+            val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!
+                .findNavController()
+            toolbar.setupWithNavController(navController, AppBarConfiguration(navController.graph))
+            setSupportActionBar(toolbar)
+            toolbar.setNavigationOnClickListener {
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
     }
 }
